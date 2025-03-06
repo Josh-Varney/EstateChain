3ca7a2c8c825c1bfb0b5d565b3acaa79f0721f47
Josh-Varney <127770606+Josh-Varney@users.noreply.github.com>
2025-03-06 17:49:22 +0000

    API communication for Admin

--- src/components/admin-portal/admin-manager/get-prop.ts
+++ src/components/admin-portal/admin-manager/get-prop.ts
@@ -1,10 +1,19 @@
 import axios from "axios";
+import { error } from "console";
 
 export const fetchProperties = async () => {
     try {
         const response = await axios.get("http://localhost:8080/get-all-unapproved");
-        return response.data.properties;
-    } catch (error){
+
+        // Check for successful status code (200-299)
+        if (response.status >= 200 && response.status < 300) {
+            return response.data;
+        } else {
+            console.log("Error fetching from API", response.statusText);
+            throw new Error("Error fetching data from API");
+        }
+
+    } catch (error) {
         console.log("Error fetching properties", error);
         return [];
     }
